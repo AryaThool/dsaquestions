f6eb2b0ab9d3405d7125b25f08e9a581832a30a5
Arya Thool <147939972+AryaThool@users.noreply.github.com>
2026-07-12 11:32:36 +0530

    Time: 89 ms (6.51%), Space: 52.4 MB (5.09%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> arrayRankTransform(vector<int>& arr) {
+//         map<int, int> numToRank;
+//         vector<int> sortedArr(arr);
+//         sort(sortedArr.begin(), sortedArr.end());
+//         int rank = 1;
+//         for (int i = 0; i < sortedArr.size(); i++) {
+//             if (i > 0 && sortedArr[i] > sortedArr[i - 1]) {
+//                 rank++;
+//             }
+//             numToRank[sortedArr[i]] = rank;
+//         }
+//         for (int i = 0; i < arr.size(); i++) {
+//             arr[i] = numToRank[arr[i]];
+//         }
+//         return arr;
+//     }
+//};
+
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int, int> numToRank;
-        vector<int> sortedArr(arr);
-        sort(sortedArr.begin(), sortedArr.end());
-        int rank = 1;
-        for (int i = 0; i < sortedArr.size(); i++) {
-            if (i > 0 && sortedArr[i] > sortedArr[i - 1]) {
-                rank++;
-            }
-            numToRank[sortedArr[i]] = rank;
-        }
+        map<int, vector<int>> numToIndices;
+
         for (int i = 0; i < arr.size(); i++) {
-            arr[i] = numToRank[arr[i]];
+            numToIndices[arr[i]].push_back(i);
+        }
+
+        int rank = 1;
+        for (auto& pair : numToIndices) {
+            for (int index : pair.second) {
+                arr[index] = rank;
+            }
+            rank++;
         }
         return arr;
     }
